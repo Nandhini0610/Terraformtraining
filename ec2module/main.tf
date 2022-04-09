37861f37052c8673805ae472a078d42f3c507ef4
Nandhini0610 <89304970+Nandhini0610@users.noreply.github.com>
2022-04-10 01:10:31 +0530

    Update main.tf

--- ec2module/main.tf
+++ ec2module/main.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "ec2instance" {
-  ami                         = data.aws_ami.myamiid.id
+  ami                         = var.ami_name
   instance_type               = var.instancetype
-  subnet_id                   = data.aws_subnet.mysubnet.id
   associate_public_ip_address = var.publicip
   availability_zone           = var.az
   tags                        = var.tag
